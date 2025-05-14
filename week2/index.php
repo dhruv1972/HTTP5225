fcb9788a3f44052bc34d8519d7fdbcd56eff2602
Dhruv Chavda <chavda.dhruv@gmail.com>
2025-05-14 16:36:45 -0400

    Added week 2 file

---
+++ week2/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>
+    <?php
+    echo "hello world!";
+    ?>
+<hr>
+    <?php
+    $fname = "Dhruv";
+    $lname = "Chavda";
+
+    echo "My name is $fname $lname.";
+    ?>
+<hr>
+<?php
+
+$people = array(
+'Jane',
+'Bill',
+'Harminder');
+$person['first'] = 'John';
+$person['last'] = 'Alexander';
+
+echo "Hello, my name is ". $person['first']. " ". $person['last']. ".";
+
+?>
+</body>
+</html> 
